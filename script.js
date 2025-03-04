9af991b3a2d47403d7aadf2fe91c2c4e29b96772
Jhielou Marie <jmsmamaradlo22@src.pshs.edu.ph>
2025-03-04 12:51:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 function checkMessage() {
     let message = prompt("Enter the message to verify:");
 
-    let result = "";
-    if (
+    if ( message !== null && message !== "") {
+        let check = 'The message "$ {value}" is ';
+        
+        if (
         (message.includes("FR") || message.includes("fr")) && 
         (message.includes("AI") || message.includes("ai"))
-    ) {
+        ) {
         result = "The message is legitimate.";
-    } else if (message.includes("FR") || message.includes("fr")) {
+            
+        } else if (message.includes("FR") || message.includes("fr")) {
         result = "The message is legitimate.";
-    } else if (message.includes("AI")) {
+
+        } else if (message.includes("AI")) {
         result = "The message is tampered with.";
-    } else if (message.includes("aI") || message.includes("Ai")) {
+            
+        } else if (message.includes("aI") || message.includes("Ai")) {
         result = "The message is tampered with.";
-    } else {
+            
+        } else {
         result = "The message is yet to be encoded.";
-    }
+            
+        }
 
     document.getElementById("result").innerText = result;
 }
