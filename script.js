b73a5701ea39f9ffedea8e250b1d4f974d4fc046
Jhielou Marie <jmsmamaradlo22@src.pshs.edu.ph>
2025-03-04 17:58:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function checkMessage() {
     } else if (messageValue.includes('AI') || messageValue.toLowerCase().includes('ai')) {
         document.getElementById('result').innerHTML = `The message "${messageValue}" is tampered!!`;
         
-    } else if (messageValue.includes('FR')) {
+    } else if (messageValue.includes('FR') || messageValue.toLowerCase().includes('fr'))  {
         document.getElementById('result').innerHTML = `The message "${messageValue}" is legitimate!`;
         
     } else {
