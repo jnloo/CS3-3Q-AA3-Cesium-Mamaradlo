f172e7643f792f26f54eb500b6e73af54d12e9e5
Jhielou Marie <jmsmamaradlo22@src.pshs.edu.ph>
2025-03-04 17:31:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,19 @@ function checkMessage() {
     
     let messageValue = prompt("Enter a word to verify: ");
 
-    if ((messageValue.indexOf('FR') >= 0) && (messageValue.indexOf('AI') >=0)) {
+    if (messageValue.includes('FR') && messageValue.includes('AI')) {
         document.getElementById('result').innerHTML = `The message "${messageValue}" is legitimate!`;
-   
-    }  
-   
-        else if (messageValue.indexOf('AI') >=0 || messageValue.indexOf('aI') >=0 || messageValue.indexOf('Ai') >=0) {
-        document.getElementById ('result').innerHTML = `The message "${messageValue}" is tampered!!`;
-   
+        
+    } else if (messageValue.includes('AI') || messageValue.toLowerCase().includes('ai')) {
+        document.getElementById('result').innerHTML = `The message "${messageValue}" is tampered!!`;
+        
+    } else if (messageValue.includes('FR')) {
+        document.getElementById('result').innerHTML = `The message "${messageValue}" is legitimate!`;
+        
+    } else {
+        document.getElementById('result').innerHTML = `The message "${messageValue}" is not yet encoded!!`;
+        
     }
-
-        else if (messageValue.indexOf('FR') >= 0) {
-        document.getElementById ('result').innerHTML = `The message "${messageValue}" is legitimate!`;
-   
-    }
-
-        else {
-        document.getElementById ('result').innerHTML = `The message "${messageValue}" is not yet encoded!!`;
-    }
-   
 }
-
 
 
